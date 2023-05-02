f3981672a82815b5a8e65e26680d2ac16fea2887
aidencjswo@gmail.com <aidencjswo@gmail.com>
2023-05-03 01:59:16 +0900

    ~622p

--- src/main/java/org/zerock/b01/domain/Board.java
+++ src/main/java/org/zerock/b01/domain/Board.java
@@ -4,6 +4,8 @@ package org.zerock.b01.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**
@@ -16,7 +18,7 @@ import javax.persistence.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@ToString(exclude = "imageSet")
 public class Board extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,5 +33,26 @@ public class Board extends BaseEntity{
     public void change(String title, String content){
         this.title = title;
         this.content = content;
+    }
+
+    @OneToMany(mappedBy = "board",
+            cascade = {CascadeType.ALL},
+            fetch = FetchType.LAZY)
+    @Builder.Default
+    private Set<BoardImage> imageSet = new HashSet<>();
+
+    public void addImage(String uuid,String fileName){
+        BoardImage boardImage = BoardImage.builder()
+                .uuid(uuid)
+                .fileName(fileName)
+                .board(this)
+                .ord(imageSet.size())
+                .build();
+        imageSet.add(boardImage);
+    }
+    public void clearImages(){
+        imageSet.forEach(boardImage -> boardImage.changeBoard(null));
+
+        this.imageSet.clear();
     }
 }
